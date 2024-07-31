8ffb94c43bc8e184daf26d5d3d2c275d76005851
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-30 20:56:35 -0700

    feat: routing, wildcard and redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {path: 'admin-component', component: AdminComponent},
-  {path: 'home-component', component: HomeComponent}
+  {path: 'home-component', component: HomeComponent},
+  {path : '', redirectTo : "/home-component", pathMatch : "full"},
+  {path: "**", component: PageNotFoundComponent}
 ];
