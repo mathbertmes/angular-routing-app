6725c142b4143b2289abd1e6dc9a98685ea39462
Matheus Bertemes <mathbertemes@gmail.com>
2024-07-30 19:18:44 -0700

    feat: 2 routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'admin-component', component: AdminComponent},
+  {path: 'home-component', component: HomeComponent}
+];
